bb91a11fe71101a6b8a48b0e014c87289aece1d0
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2021-05-24 15:41:02 +0430

    AddMarkerClass implement in a seperate class

--- app/src/main/java/com/example/startwithneshan/MapActivity.java
+++ app/src/main/java/com/example/startwithneshan/MapActivity.java
@@ -43,57 +43,18 @@ public class MapActivity extends AppCompatActivity {
         initLayoutReferences();
     }
 
-
     // We use findViewByID for every element in our layout file here
     private void initViews(){
-        map = findViewById(R.id.map);
+//        map = findViewById(R.id.map);
     }
     // Initializing layout references (views, map and map events)
     private void initLayoutReferences() {
         // Initializing views
-        initViews();
+        //initViews();
+        AddMarkerClass addMarkerClass = new AddMarkerClass(getApplicationContext(), map);
+        addMarkerClass.initLayoutReferences();
 
-        // when clicked on map, a marker is added in clicked location
-        map.setOnMapClickListener(latLng -> {
 
-            //set marker on each location that user clicked on it
-            map.addMarker(createMarker(latLng));
-        });
-        // when on marker clicked, change marker style to blue
-        map.setOnMarkerClickListener(marker1 -> {
-            //changeMarkerToBlue(marker1);
-        });
     }
 
-    // This method gets a LatLng as input and adds a marker on that position
-    private Marker createMarker(LatLng loc) {
-
-        // Creating animation for marker. We should use an object of type AnimationStyleBuilder, set
-        // all animation features on it and then call buildStyle() method that returns an object of type
-        // AnimationStyle
-        AnimationStyleBuilder animStBl = new AnimationStyleBuilder();
-        animStBl.setFadeAnimationType(AnimationType.ANIMATION_TYPE_SMOOTHSTEP);
-        animStBl.setSizeAnimationType(AnimationType.ANIMATION_TYPE_SPRING);
-        animStBl.setPhaseInDuration(0.5f);
-        animStBl.setPhaseOutDuration(0.5f);
-        animSt = animStBl.buildStyle();
-
-        // Creating marker style. We should use an object of type MarkerStyleCreator, set all features on it
-        // and then call buildStyle method on it. This method returns an object of type MarkerStyle
-        MarkerStyleBuilder markStCr = new MarkerStyleBuilder();
-        markStCr.setSize(30f);
-        markStCr.setBitmap(BitmapUtils.createBitmapFromAndroidBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_marker)));
-        // AnimationStyle object - that was created before - is used here
-        markStCr.setAnimationStyle(animSt);
-        MarkerStyle markSt = markStCr.buildStyle();
-
-        // Creating marker
-        return new Marker(loc, markSt);
-    }
-
-    /***
-     * for removing marker from map we use :
-     * map.removeMarker(marker);
-     *
-     */
 }
