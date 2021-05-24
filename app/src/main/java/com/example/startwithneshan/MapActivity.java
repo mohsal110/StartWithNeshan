b2e15b8ce477731d1487ab969362682de2ccfccd
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2021-05-24 15:00:16 +0430

    add marker methods is created

--- app/src/main/java/com/example/startwithneshan/MapActivity.java
+++ app/src/main/java/com/example/startwithneshan/MapActivity.java
@@ -2,19 +2,98 @@ package com.example.startwithneshan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.carto.styles.AnimationStyle;
+import com.carto.styles.AnimationStyleBuilder;
+import com.carto.styles.AnimationType;
+import com.carto.styles.MarkerStyle;
+import com.carto.styles.MarkerStyleBuilder;
+import com.carto.utils.BitmapUtils;
+
+import org.neshan.common.model.LatLng;
 import org.neshan.mapsdk.MapView;
+import org.neshan.mapsdk.model.Marker;
 
 public class MapActivity extends AppCompatActivity {
 
 
 
     MapView map;
+    AnimationStyle animSt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         map=findViewById(R.id.map);
+
+        ///starting app in full screen
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // everything related to ui is initialized here
+        initLayoutReferences();
+    }
+
+
+    // We use findViewByID for every element in our layout file here
+    private void initViews(){
+        map = findViewById(R.id.map);
+    }
+    // Initializing layout references (views, map and map events)
+    private void initLayoutReferences() {
+        // Initializing views
+        initViews();
+
+        // when clicked on map, a marker is added in clicked location
+        map.setOnMapClickListener(latLng -> {
+
+            //set marker on each location that user clicked on it
+            map.addMarker(createMarker(latLng));
+        });
+        // when on marker clicked, change marker style to blue
+        map.setOnMarkerClickListener(marker1 -> {
+            //changeMarkerToBlue(marker1);
+        });
+    }
+
+    // This method gets a LatLng as input and adds a marker on that position
+    private Marker createMarker(LatLng loc) {
+
+        // Creating animation for marker. We should use an object of type AnimationStyleBuilder, set
+        // all animation features on it and then call buildStyle() method that returns an object of type
+        // AnimationStyle
+        AnimationStyleBuilder animStBl = new AnimationStyleBuilder();
+        animStBl.setFadeAnimationType(AnimationType.ANIMATION_TYPE_SMOOTHSTEP);
+        animStBl.setSizeAnimationType(AnimationType.ANIMATION_TYPE_SPRING);
+        animStBl.setPhaseInDuration(0.5f);
+        animStBl.setPhaseOutDuration(0.5f);
+        animSt = animStBl.buildStyle();
+
+        // Creating marker style. We should use an object of type MarkerStyleCreator, set all features on it
+        // and then call buildStyle method on it. This method returns an object of type MarkerStyle
+        MarkerStyleBuilder markStCr = new MarkerStyleBuilder();
+        markStCr.setSize(30f);
+        markStCr.setBitmap(BitmapUtils.createBitmapFromAndroidBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_marker)));
+        // AnimationStyle object - that was created before - is used here
+        markStCr.setAnimationStyle(animSt);
+        MarkerStyle markSt = markStCr.buildStyle();
+
+        // Creating marker
+        return new Marker(loc, markSt);
+    }
+
+    /***
+     * for removing marker from map we use :
+     * map.removeMarker(marker);
+     *
+     */
 }
